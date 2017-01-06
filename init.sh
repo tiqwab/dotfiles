8ac817d62b1b07792b0a8ad1b399c6e81225ab25
tiqwab <tiqwab.ch90@gmail.com>
2017-01-06 11:59:12 +0900

    Install neobundle

--- init.sh
+++ init.sh
@@ -3,3 +3,7 @@
 # Initialize (install plugins, bundles, etc.)
 
 echo "Start initialization"
+
+# Install NeoBundle
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > /var/tmp/install_neobundle.sh
+sh /var/tmp/install_neobundle.sh
